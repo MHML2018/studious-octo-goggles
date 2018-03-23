663fe1a8e3ea0604e89be7b53b780be37d2d80e8
jonathan <jr2014@ic.ac.uk>
2018-03-23 19:45:57 +0000

    Added early stopping and testtrain splitter

--- training_script.py
+++ training_script.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import *
 from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
 import numpy as np
 #6import pickle
 # fix random seed for reproducibility
@@ -10,7 +11,16 @@ import numpy as np
 # load pima indians dataset
 dataset = np.loadtxt("DataSetV4/data.csv", delimiter=",")
 # split into input (X) and output (Y) variables
-X = dataset[:,0:13]
+
+np.random.shuffle(dataset)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+dataset_train, dataset_test = train_test_split(dataset, test_size=0.2)
+
+X = dataset_train[:,0:13]
+
 meanz = np.mean(X)
 maxz = np.max(X)
 minz = np.min(X)
@@ -19,24 +29,37 @@ X = X/(maxz - minz)
 
 num_classes = 6
 
-Y = dataset[:,13]
+Y = dataset_train[:,13]
 Y = to_categorical(Y, num_classes)
 # create model
 #print(X)
 #print(Y)
 model = Sequential()
-model.add(Dense(18, input_dim=13, activation='tanh'))
+model.add(Dense(20, input_dim=13, activation='tanh'))
 model.add(Dense(20, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(20, activation='relu'))
 model.add(Dense(num_classes, activation='sigmoid'))
 # Compile model
 model.compile(loss='categorical_crossentropy',
               optimizer=RMSprop(),
               metrics=['accuracy'])
+              
+earlystop = EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=50, verbose=1, mode='auto')
+callbacks_list = [earlystop]
 			  
-model.fit(X, Y, epochs=200, batch_size=100, verbose=1)
+model.fit(X, Y, epochs=1000, batch_size=200, callbacks=callbacks_list, verbose=1, validation_split=0.2)
 # evaluate the model
-scores = model.evaluate(X, Y)
+
+X_test = dataset_test[:,0:13]
+X_test = X_test-meanz
+X_test = X_test/(maxz - minz)
+Y_test = dataset_test[:,13]
+Y_test = to_categorical(Y_test, num_classes)
+
+Y_pred=model.predict(X_test)
+conf_matx = confusion_matrix(Y_test.argmax(axis=1), Y_pred.argmax(axis=1))
+
+scores = model.evaluate(X_test, Y_test)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 # serialize model to JSON
@@ -51,3 +74,6 @@ print("Norm-Parms - mean, max, min")
 print(meanz)
 print(maxz)
 print(minz)
+
+print(conf_matx)
+
